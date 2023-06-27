43d407d71367aa743aae6f249031b29c5e9128b0
ZingaG <trad_bar@mail.ru>
2023-06-27 16:58:41 +0300

    Task 2 completed

--- program.java
+++ program.java
@@ -16,13 +16,14 @@ public class program {
             int task_n = Integer.parseInt(scaner.nextLine());
             if(task_n == 1){
                 task1();
+            } else if(task_n == 2){
+                task2();
             }
         }
-
-        
     }
 
     static void task1(){
+
         System.out.println("Enter n = ");
         int n = Integer.parseInt(scaner.nextLine());
         int res = 0;
@@ -37,6 +38,28 @@ public class program {
         System.out.printf("Sum 1 to n = %d", res);
         System.out.println();
         System.out.printf("Multiply 1 to n = %d", res2);
+
+    }
+
+    static void task2(){
+
+        for (int n = 2; n < 1001; n++) {
+
+            boolean flag = true;
+
+            for (int i = 2; i <= Math.sqrt(n) ; i++) {
+
+                if (n % i == 0){
+                    flag = false;
+                    break;
+                }
+
+            }
+
+            if(flag)
+                System.out.println(n);
+
+        }
 
     }
     
