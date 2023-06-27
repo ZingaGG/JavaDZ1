1b046c3a413cf43a33fb834a4e012c73d55ab857
ZingaG <trad_bar@mail.ru>
2023-06-27 17:28:32 +0300

    Task 3 completed

--- program.java
+++ program.java
@@ -5,21 +5,22 @@ public class program {
     static Scanner scaner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        boolean f = true;
-        while(f) {
-            System.out.println("Task 1 - 1");
-            System.out.println("Task 2 - 2");
-            System.out.println("Task 3 - 3");
-            System.out.println("Task 4 - 4");
-            System.out.println("Exit - 5");
-            System.out.println("Enter task number = ");
-            int task_n = Integer.parseInt(scaner.nextLine());
-            if(task_n == 1){
-                task1();
-            } else if(task_n == 2){
-                task2();
-            }
-        }
+        // boolean f = true;
+        // while(f) {
+        //     System.out.println("Task 1 - 1");
+        //     System.out.println("Task 2 - 2");
+        //     System.out.println("Task 3 - 3");
+        //     System.out.println("Task 4 - 4");
+        //     System.out.println("Exit - 5");
+        //     System.out.println("Enter task number = ");
+        //     int task_n = Integer.parseInt(scaner.nextLine());
+        //     if(task_n == 1){
+        //         task1();
+        //     } else if(task_n == 2){
+        //         task2();
+        //     }
+        // }
+        System.out.println(task3());
     }
 
     static void task1(){
@@ -44,22 +45,49 @@ public class program {
     static void task2(){
 
         for (int n = 2; n < 1001; n++) {
-
             boolean flag = true;
-
             for (int i = 2; i <= Math.sqrt(n) ; i++) {
-
                 if (n % i == 0){
                     flag = false;
                     break;
                 }
-
             }
-
             if(flag)
                 System.out.println(n);
-
         }
+    }
+
+    static Object task3(){
+        System.out.println("Enter first digit = ");
+        System.out.println();
+
+        int first = Integer.parseInt(scaner.nextLine());
+
+        System.out.println("Enter second digit = ");
+        System.out.println();
+
+        int second = Integer.parseInt(scaner.nextLine());
+
+        System.out.println("Enter option");
+        System.out.println("Sum - '+'");
+        System.out.println("Difference - '-'");
+        System.out.println("Multiply - '*'");
+        System.out.println("Sum - '/'");
+
+        String option = scaner.nextLine();
+
+
+        if (option.equals("+")) {
+            return first + second;
+        } else if (option.equals("-")) {
+                return first - second;
+        } else if (option.equals("*")) {
+            return first * second;
+        } else if (option.equals("/")) {
+            return (double) first / second;
+        }
+
+        return null;
 
     }
     
